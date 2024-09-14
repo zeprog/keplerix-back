0ec4126144927d893151d7208c2e8b7e66d289cc
Alina <dmitrium02@mail.ru>
2024-09-14 20:49:21 +0300

    Add a verify account and reset password

--- app/api/main.py
+++ app/api/main.py
@@ -20,6 +20,7 @@ def create_app() -> FastAPI:
         [auth_backend],
     )
 
+    # POST
     app.include_router(
         fastapi_users.get_auth_router(auth_backend),
         prefix="/auth",
@@ -28,6 +29,18 @@ def create_app() -> FastAPI:
 
     app.include_router(
         fastapi_users.get_register_router(UserRead, UserCreate),
+        prefix="/auth",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        fastapi_users.get_verify_router(UserRead),
+        prefix="/auth",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        fastapi_users.get_reset_password_router(),
         prefix="/auth",
         tags=["auth"],
     )
--- app/core/manager.py
+++ app/core/manager.py
@@ -1,30 +1,37 @@
-import uuid
+import secrets
 from typing import Optional
 from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
+from fastapi_users import BaseUserManager, IntegerIDMixin
+from fastapi_users.authentication import JWTStrategy
 from db.models.user import Users
 from db.session import get_user_db
 
-SECRET = "SECRET"
-
-
 class UserManager(IntegerIDMixin, BaseUserManager[Users, int]):
-    reset_password_token_secret = SECRET
-    verification_token_secret = SECRET
+    verification_token_secret = secrets.token_urlsafe(32)
+    reset_password_token_secret = secrets.token_urlsafe(32)
+    
+    async def validate_verification_token(self, token: str) -> Optional[Users]:
+        payload = JWTStrategy(secret=self.verification_token_secret).decode(token)
+        user_id = payload.get("sub")
+        return await self.user_db.get(user_id)
+
+    async def validate_reset_password_token(self, token: str) -> Optional[Users]:
+        payload = JWTStrategy(secret=self.reset_password_token_secret).decode(token)
+        user_id = payload.get("sub")
+        return await self.user_db.get(user_id)
 
     async def on_after_register(self, user: Users, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
 
-    # async def on_after_forgot_password(
-    #     self, user: User, token: str, request: Optional[Request] = None
-    # ):
-    #     print(f"User {user.id} has forgot their password. Reset token: {token}")
+    async def on_after_forgot_password(
+        self, user: Users, token: str, request: Optional[Request] = None
+    ):
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
 
-    # async def on_after_request_verify(
-    #     self, user: User, token: str, request: Optional[Request] = None
-    # ):
-    #     print(f"Verification requested for user {user.id}. Verification token: {token}")
-
+    async def on_after_request_verify(
+        self, user: Users, token: str, request: Optional[Request] = None
+    ):
+        print(f"Verification requested for user {user.id}. Verification token: {token}")
 
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
